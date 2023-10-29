9513dae72556ec7f38381fc65ef9ddcd91a87e9f
IreneHuang23 <iirene.huang@mail.utoronto.ca>
2023-10-29 16:44:24 -0400

    message private

--- src/entity/Message.java
+++ src/entity/Message.java
@@ -1,4 +1,13 @@
 package entity;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class Message {
+    private String user_id;
+    private String message;
+    private String channel_type;
+    private String channel_url;
+    private String data;
+    private boolean send_push;
 }
