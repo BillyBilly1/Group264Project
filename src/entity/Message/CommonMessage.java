1eeb39e60aabaa779f7ca716a717e5ab3a22a683
IreneHuang23 <iirene.huang@mail.utoronto.ca>
2023-10-30 20:11:10 -0400

    update CommonMessage

--- src/entity/Message/CommonMessage.java
+++ src/entity/Message/CommonMessage.java
@@ -1,4 +1,81 @@
-package Message;
+package entity.Message;
 
-public class CommonMessage {
+public class CommonMessage implements Message {
+    private final String user_id;
+    private String message;
+    private String channel_type;
+    private String channel_url;
+    private String data;
+    private boolean send_push;
+
+
+    public CommonMessage(String userId, String message, String channelType, String channelUrl, String data, boolean sendPush) {
+        this.user_id = userId;
+        this.message = message;
+        this.channel_type = channelType;
+        this.channel_url = channelUrl;
+        this.data = data;
+        this.send_push = sendPush;
+    }
+
+    public static CommonMessageBuilder builder() { return new CommonMessageBuilder();}
+
+    public static class CommonMessageBuilder {
+        private String user_id;
+        private String message;
+        private String channel_type;
+        private String channel_url;
+        private String data;
+        private boolean send_push;
+
+        public CommonMessageBuilder user_id(String id) {
+            this.user_id = id;
+            return this;
+        }
+
+        public CommonMessageBuilder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public CommonMessageBuilder channelType(String channelType) {
+            this.channel_type = channelType;
+            return this;
+        }
+
+        public CommonMessageBuilder channelUrl(String channelUrl) {
+            this.channel_url = channelUrl;
+            return this;
+        }
+
+        public CommonMessageBuilder data(String data) {
+            this.data = data;
+            return this;
+        }
+
+        public CommonMessageBuilder sendPush(Boolean sendPush) {
+            this.send_push = sendPush;
+            return this;
+        }
+
+        public CommonMessage build() {
+            return new CommonMessage(user_id, message, channel_type, channel_url,
+                    data, send_push);
+        }
+    }
+
+
+    public String getUser_id() {return user_id;};
+
+    public String getMessage() {return message;};
+
+    public String getChannel_type() {return channel_type;};
+
+    public  String getChannel_url() {return channel_url;};
+
+    public String getData() {return data;};
+
+    public Boolean getSendPush() {return send_push;};
+}
+
 }
--- src/entity/Message/Message.java
+++ src/entity/Message/Message.java
@@ -1,4 +1,15 @@
 package Message;
 
 public interface Message {
+    String getUser_id();
+
+    String getMessage();
+
+    String getChannel_type();
+
+    String getChannel_url();
+
+    String getData();
+
+    Boolean getSendPush();
 }
