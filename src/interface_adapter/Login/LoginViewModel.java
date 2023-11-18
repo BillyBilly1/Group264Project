3bc598b2796715bafd51857af695299e006b6d94
IreneHuang23 <iirene.huang@mail.utoronto.ca>
2023-11-18 18:28:59 -0500

    LoginViewModel version 1, make more modification after communicate with team member about ViewModel and logged_in

--- src/interface_adapter/Login/LoginViewModel.java
+++ src/interface_adapter/Login/LoginViewModel.java
@@ -1,4 +1,35 @@
 package interface_adapter.Login;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class LoginViewModel {
+
+    public final String LOGIN_TITLE = "Log in View";
+    public final String USER_ID_LABEL = "Enter User_id";
+    public final String NKCKNAME_LABEL = "Enter Nickname";
+    public static final String LOGIN_BUTTON_LABEL = "log in";
+    public static final String CANCEL_BUTTON_LABEL = "Cancel";
+
+    private LoginState state = new LoginState();
+
+    //public LoginViewModel() {super("log in");}
+
+    public void setState(LoginState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertychanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangedListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public LoginState getState() {
+        return state;
+    }
 }
