729d3d8ebda74ef8815d016e61262dae88d7d62c
IreneHuang23 <iirene.huang@mail.utoronto.ca>
2023-11-18 17:12:25 -0500

    complete LoginState version 1, modify if further need

--- src/interface_adapter/Login/LoginState.java
+++ src/interface_adapter/Login/LoginState.java
@@ -1,4 +1,39 @@
 package interface_adapter.Login;
 
 public class LoginState {
+    private String user_id = "";
+    private String user_idError = null;
+    private String nickname = "";
+
+    public LoginState(LoginState copy) {
+        user_id = copy.user_id;
+        user_idError = copy.user_idError;
+        nickname = copy.nickname;
+    }
+
+    public LoginState() {}
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public String getUser_idError() {
+        return user_idError;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setUser_idError(String user_idError) {
+        this.user_idError = user_idError;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
