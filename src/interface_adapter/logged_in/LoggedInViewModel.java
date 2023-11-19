6b46a8a6917e92766e852f9a03803daf2fa3e5dd
IreneHuang23 <iirene.huang@mail.utoronto.ca>
2023-11-18 23:01:14 -0500

    cpmpleted LoggedInViewModel

--- src/interface_adapter/logged_in/LoggedInViewModel.java
+++ src/interface_adapter/logged_in/LoggedInViewModel.java
@@ -1,4 +1,48 @@
 package interface_adapter.logged_in;
 
-public class LoggedInViewModel {
+import interface_adapter.ViewModel;
+import interface_adapter.Login.LoginState;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class LoggedInViewModel extends ViewModel{
+    public final String TITLE_LABEL = "Logged in View";
+
+    private LoggedInState state = new LoggedInState();
+
+    public static final String LOGOUT_BUTTON_LABEL = "Log out";
+    private String loggedInUser;
+
+    public LoggedInViewModel(String viewName) {
+        super("logged in");
+    }
+
+    public void setState(LoggedInState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangedListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public LoggedInState getState() {
+        return state;
+    }
+
+    public String getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public void setLoggedInUser(String loggedInUser) {
+        this.loggedInUser = loggedInUser;
+    }
 }
