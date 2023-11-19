32e653c88f861791d269bb24c7382bc045ce24a6
IreneHuang23 <iirene.huang@mail.utoronto.ca>
2023-11-18 23:06:18 -0500

    cpmpleted LoginPresenter

--- src/interface_adapter/Login/LoginPresenter.java
+++ src/interface_adapter/Login/LoginPresenter.java
@@ -1,4 +1,35 @@
 package interface_adapter.Login;
 
-public class LoginPresenter {
+import interface_adapter.logged_in.LoggedInState;
+import use_case.Login.LoginOutputBoundary;
+import use_case.Login.LoginOutputData;
+import interface_adapter.logged_in.LoggedInViewModel;
+
+
+public class LoginPresenter implements LoginOutputBoundary{
+
+    private final LoginViewModel loginViewModel;
+
+    private final LoggedInViewModel loggedInViewModel;
+
+    public LoginPresenter(LoginViewModel loginViewModel, LoggedInViewModel loggedInViewModel) {
+        this.loginViewModel = loginViewModel;
+        this.loggedInViewModel = loggedInViewModel;
+    }
+
+
+    @Override
+    public void prepareSuccessView(LoginOutputData user) {
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUser_id(user.getUser_id());
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUser_idError(error);
+        loginViewModel.firePropertyChanged();
+    }
 }
