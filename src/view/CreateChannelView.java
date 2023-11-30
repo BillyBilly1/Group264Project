7d37e3a089957c37b11292aadaa53cd5f032a35f
BillyBilly1 <yidali.li@mail.utoronto.ca>
2023-11-29 23:15:56 -0500

    I made minor modifications to createchannelview, addressing the issue where the textfield was covering the label. I also added a main function for preview purposes. Please note that the main function will be removed in the future.

--- src/view/CreateChannelView.java
+++ src/view/CreateChannelView.java
@@ -1,6 +1,5 @@
 package view;
 
-import interface_adapter.Login.LoginViewModel;
 import interface_adapter.create_channel.CreateChannelController;
 import interface_adapter.create_channel.CreateChannelViewModel;
 
@@ -15,68 +14,55 @@ public class CreateChannelView extends JPanel implements ActionListener, Propert
     public final String viewName = "create channel";
 
     private final CreateChannelViewModel createChannelViewModel;
-
     private final CreateChannelController createChannelController;
-
     final JTextField channelIDINFO = new JTextField();
-
     final JTextField channelNameINFO = new JTextField();
-
     private final JButton createButton;
 
     public CreateChannelView(CreateChannelViewModel createChannelViewModel, CreateChannelController createChannelController) {
         this.createChannelViewModel = createChannelViewModel;
         this.createChannelController = createChannelController;
 
-        JFrame frame = new JFrame("YouChat - Login");
+        this.setLayout(null);
+
+        JLabel titleLabel = new JLabel(CreateChannelViewModel.CREATE_CHANNEL_TITLE, SwingConstants.CENTER);
+        titleLabel.setBounds(100, 20, 200, 30); // 修改 titleLabel 的 bounds
+        this.add(titleLabel);
+
+        JLabel channelIDLabel = new JLabel(CreateChannelViewModel.CHANNEL_ID_LABEL);
+        channelIDLabel.setBounds(10, 70, 180, 25); // 修改 channelIDLabel 的 bounds
+        this.add(channelIDLabel);
+
+        channelIDINFO.setBounds(200, 70, 275, 25); // 修改 channelIDINFO 的 bounds
+        this.add(channelIDINFO);
+
+        JLabel channelNameLabel = new JLabel(CreateChannelViewModel.CHANNEL_NAME_LABEL);
+        channelNameLabel.setBounds(10, 120, 180, 25); // 修改 channelNameLabel 的 bounds
+        this.add(channelNameLabel);
+
+        channelNameINFO.setBounds(200, 120, 275, 25); // 修改 channelNameINFO 的 bounds
+        this.add(channelNameINFO);
+
+        createButton = new JButton(CreateChannelViewModel.CREATE_CHANNEL_BUTTON_LABLE);
+        createButton.setBounds(150, 170, 100, 40); // 修改 createButton 的 bounds
+        createButton.addActionListener(this);
+        this.add(createButton);
+
+        // 设置 JFrame
+        JFrame frame = new JFrame("YouChat - Create Channel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 600);
-
-        JButton createButton = new JButton(CreateChannelViewModel.CREATE_CHANNEL_BUTTON_LABLE);
-        createButton.setBounds((400 / 2) - (100 / 2), 300, 100, 40);
-        this.createButton = createButton;
-
-        JPanel panel = new JPanel();
-        frame.add(panel);
-        placeComponents(panel);
-
+        frame.setSize(500, 300);
+        frame.add(this);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
-    private void placeComponents(JPanel panel) {
 
-        panel.setLayout(null);
+    public static void main(String[] args) {
 
-        JLabel titleLabel = new JLabel(CreateChannelViewModel.CREATE_CHANNEL_TITLE, SwingConstants.CENTER);
-        titleLabel.setBounds(150, 100, 100, 30);
-        panel.add(titleLabel);
-
-        JLabel channelIDLabel = new JLabel(CreateChannelViewModel.CHANNEL_ID_LABEL);
-        channelIDLabel.setBounds(10, 150, 150, 30);
-        panel.add(channelIDLabel);
-
-        JTextField channelID = channelIDINFO;
-        channelID.setBounds(100, 150, 250, 40);
-        panel.add(channelID);
-
-        JLabel channelNameLabel = new JLabel(CreateChannelViewModel.CHANNEL_NAME_LABEL);
-        channelNameLabel.setBounds(10, 200, 150, 30);
-        panel.add(channelNameLabel);
-
-        JTextField channelName = this.channelNameINFO;
-        channelName.setBounds(100, 200, 250, 40);
-        panel.add(channelName);
-
-
-        panel.add(createButton);
-
-
-        createButton.addActionListener(this);
+        //测试用，后续请删除。
+        new CreateChannelView(new CreateChannelViewModel(), new CreateChannelController());
     }
-
-
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -84,11 +70,9 @@ public class CreateChannelView extends JPanel implements ActionListener, Propert
         if (e.getSource() == createButton) {
             createChannelController.execute();
         }
-
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
     }
 }
