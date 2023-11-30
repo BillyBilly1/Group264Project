593012e85c4f2eaf304b4ab2e70a88d85f6106ad
BillyBilly1 <yidali.li@mail.utoronto.ca>
2023-11-30 18:48:49 -0500

    MenuViewmodel, contains a method of getChannelNameList

--- src/interface_adapter/Menu/MenuViewModel.java
+++ src/interface_adapter/Menu/MenuViewModel.java
@@ -1,2 +1,28 @@
-package interface_adapter.Menu;public class MenuViewModel {
-}
+package interface_adapter.Menu;
+
+import entity.Channel.Channel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuViewModel {
+
+    private ArrayList<Channel> channelList;
+
+    private String userID;
+
+
+    public String getUserID() {
+        return this.userID;
+    }
+
+
+    public List<String> getChannelNameList() {
+        ArrayList<String> nameList = new ArrayList<String>();
+        for (Channel channel : channelList) {
+            nameList.add(channel.getChannelName());}
+        return nameList;
+
+
+        }
+    }
