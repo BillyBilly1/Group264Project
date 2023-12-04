9e66b7f52287d4716226f48e5289670015c8b201
BillyBilly1 <yidali.li@mail.utoronto.ca>
2023-12-04 00:32:12 -0500

    add a pop-up according to the request of yandex api, and shows a hyperlink which will lead us to yandex api webset

--- src/interface_adapter/translation/TranslatePresenter.java
+++ src/interface_adapter/translation/TranslatePresenter.java
@@ -4,6 +4,12 @@ import interface_adapter.Channel.ChannelViewModel;
 import use_case.translate.TranslateOutputBoundary;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class TranslatePresenter implements TranslateOutputBoundary {
 
@@ -16,20 +22,43 @@ public class TranslatePresenter implements TranslateOutputBoundary {
         this.translateViewModel = translateViewModel;
     }
 
-
     @Override
     public void presentTranslationResult(TranslateState state) {
-
         if (state.isTranslationSuccessful()) {
             channelViewModel.updateTranslatedText(translateViewModel.getState().getTranslatedText());
-        }
 
-        else {
+            // 创建包含可点击链接的 JPanel
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+
+            // 创建包含链接的标签
+            JLabel label = new JLabel("<html><a href='https://translate.yandex.com/'>Translated by Yandex Translate</a></html>");
+            label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+            // 添加链接点击事件监听器
+            label.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    openWebPage("https://translate.yandex.com/");
+                }
+            });
+
+            // 将标签添加到面板
+            panel.add(label, BorderLayout.CENTER);
+
+            // 显示包含链接的消息框
+            JOptionPane.showMessageDialog(null, panel, "Translation Result", JOptionPane.PLAIN_MESSAGE);
+        } else {
             JOptionPane.showMessageDialog(null, "Failed: " + state.getError(),
-                    "Translation Erro", JOptionPane.ERROR_MESSAGE);
-
+                    "Translation Error", JOptionPane.ERROR_MESSAGE);
         }
+    }
 
-
+    private void openWebPage(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException ex) {
+            ex.printStackTrace();
+        }
     }
 }
